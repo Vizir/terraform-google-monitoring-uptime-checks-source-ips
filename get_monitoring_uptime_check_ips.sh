e310cc531641f9ade7ee3f98db1a1ffa1bf782e7
Joao Kiyoshi <joao@vizir.com.br>
2019-05-30 21:25:20 -0300

    Fix script output formatting

--- get_monitoring_uptime_check_ips.sh
+++ get_monitoring_uptime_check_ips.sh
@@ -5,8 +5,7 @@ set -e
 ips=$( \
   curl -sSL https://monitoring.googleapis.com/v3/uptimeCheckIps \
     -H "Authorization: Bearer $(gcloud auth print-access-token)" \
-  | jq -c '.uptimeCheckIps | map(.ipAddress + "/32") | join(",")'
+  | jq -r '.uptimeCheckIps | map(.ipAddress + "/32") | join(",")'
 )
 
 jq -n --arg ips "$ips" '{"ips": $ips}'
-
